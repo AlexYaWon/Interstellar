02c8f29967c39c8a56072466a3a6a1de221d2e00
AlexYaWon <110936985+AlexYaWon@users.noreply.github.com>
2024-03-17 23:06:26 -0400

    Update frame.js

--- static/assets/scripts/frame.js
+++ static/assets/scripts/frame.js
@@ -35,8 +35,8 @@ function popout() {
   const newWindow = window.open('about:blank', '_blank')
 
   if (newWindow) {
-    const name = localStorage.getItem('name') || 'Dashboard'
-    const icon = localStorage.getItem('icon') || 'https://www.instructure.com/sites/default/files/image/2021-12/Canvas_logo_single_mark.png'
+    const name = localStorage.getItem('name') || 'New Tab'
+    const icon = localStorage.getItem('icon') || ''
 
     newWindow.document.title = name
 
